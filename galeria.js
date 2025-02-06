938eaad0387a3bd43949883711bbf83cbac2e97e
DonatoRM <donatoramos@hotmail.com>
2025-02-06 11:07:00 +0100

    galeria inicial

---
+++ galeria.js
@@ -0,0 +1,30 @@
+'use strict';
+// Crearemos un array con la galería de imágenes
+const images = [
+  'https://picsum.photos/600/400?text=Imagen+1',
+  'https://picsum.photos/600/400?text=Imagen+2',
+  'https://picsum.photos/600/400?text=Imagen+3',
+  'https://picsum.photos/600/400?text=Imagen+4',
+  'https://picsum.photos/600/400?text=Imagen+5',
+  'https://picsum.photos/600/400?text=Imagen+6',
+];
+// Creamos los elementos del DOM
+// galería, modal, modalImage, close, next, prev
+const gallery = document.getElementById('gallery');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const closeBtn = document.getElementById('close');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+let currentIndex = 0; // Índice de la imagen actual
+// Creación de la función de la Galería
+const crearGaleria = () => {
+  images.forEach((imagen, index) => {
+    const img = document.createElement('img');
+    img.src = imagen;
+    img.alt = `Imagen ${i + 1}`;
+    img.addEventListener('click', () => openModal(index));
+    gallery.appendChild(img);
+  });
+};
