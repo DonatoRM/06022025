72aeaa3a495c002cc23c487602638beae6826437
DonatoRM <donatoramos@hotmail.com>
2025-02-06 12:59:05 +0100

    Modificación 2

--- galeria.js
+++ galeria.js
@@ -1,12 +1,12 @@
 'use strict';
 // Crearemos un array con la galería de imágenes
 const images = [
-  'https://picsum.photos/600/400?text=Imagen+1',
-  'https://picsum.photos/600/400?text=Imagen+2',
-  'https://picsum.photos/600/400?text=Imagen+3',
-  'https://picsum.photos/600/400?text=Imagen+4',
-  'https://picsum.photos/600/400?text=Imagen+5',
-  'https://picsum.photos/600/400?text=Imagen+6',
+    'https://picsum.photos/600/400?text=Imagen+1',
+    'https://picsum.photos/600/400?text=Imagen+2',
+    'https://picsum.photos/600/400?text=Imagen+3',
+    'https://picsum.photos/600/400?text=Imagen+4',
+    'https://picsum.photos/600/400?text=Imagen+5',
+    'https://picsum.photos/600/400?text=Imagen+6',
 ];
 // Creamos los elementos del DOM
 // galería, modal, modalImage, close, next, prev
@@ -19,22 +19,50 @@ const nextBtn = document.getElementById('next');
 let currentIndex = 0; // Índice de la imagen actual
 // Creación de la función de la Galería
 const crearGaleria = () => {
-  images.forEach((imagen, index) => {
-    const img = document.createElement('img');
-    img.src = imagen;
-    img.alt = `Imagen ${index + 1}`;
-    img.addEventListener('click', openModal(index));
-    gallery.appendChild(img);
-  });
+    images.forEach((imagen, index) => {
+        const img = document.createElement('img');
+        img.src = imagen;
+        img.alt = `Imagen ${index + 1}`;
+        console.log(index);
+        img.addEventListener('click', () => openModal(index));
+        gallery.appendChild(img);
+    });
 };
 // Función para abrir el modal y mostrar la imagen seleccionada
-const openModal = index => {
-  currentIndex = index;
-  modalImage.src = images[currentIndex];
-  modal.style.display = 'flex';
-};
+function openModal(index) {
+    currentIndex = index;
+    modalImage.src = images[currentIndex];
+    modal.style.display = 'flex';
+}
 // Función para cerrar el modal
 const closeModal = () => {
-  modal.style.display = 'none';
+    modal.style.display = 'none';
 };
+function prevImagen() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = images.length - 1; // Volver al final si es la primera imagen
+    }
+    modalImage.src = images[currentIndex];
+}
+function nextImagen() {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Volver al principio si es la última imagen
+    }
+    modalImage.src = images[currentIndex];
+}
+// Eventos de los EventListeners
+closeBtn.addEventListener('click', closeModal);
+prevBtn.addEventListener('click', prevImagen);
+nextBtn.addEventListener('click', nextBtn);
+
+windows.addEventListener('click', event => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
 crearGaleria();
