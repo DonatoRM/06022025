2f2c4c2ec93b699aa331d581f2aa508abcb4f289
DonatoRM <donatoramos@hotmail.com>
2025-02-06 12:28:57 +0100

    modificación 1

--- galeria.js
+++ galeria.js
@@ -10,21 +10,31 @@ const images = [
 ];
 // Creamos los elementos del DOM
 // galería, modal, modalImage, close, next, prev
-const gallery = document.getElementById('gallery');
+const gallery = document.getElementById('galeria');
 const modal = document.getElementById('modal');
 const modalImage = document.getElementById('modal-image');
 const closeBtn = document.getElementById('close');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
-
 let currentIndex = 0; // Índice de la imagen actual
 // Creación de la función de la Galería
 const crearGaleria = () => {
   images.forEach((imagen, index) => {
     const img = document.createElement('img');
     img.src = imagen;
-    img.alt = `Imagen ${i + 1}`;
-    img.addEventListener('click', () => openModal(index));
+    img.alt = `Imagen ${index + 1}`;
+    img.addEventListener('click', openModal(index));
     gallery.appendChild(img);
   });
 };
+// Función para abrir el modal y mostrar la imagen seleccionada
+const openModal = index => {
+  currentIndex = index;
+  modalImage.src = images[currentIndex];
+  modal.style.display = 'flex';
+};
+// Función para cerrar el modal
+const closeModal = () => {
+  modal.style.display = 'none';
+};
+crearGaleria();
